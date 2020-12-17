e55f6a5506ef9b86c5e11f59671cfa66bc010583
Ghost4dot2 <ngroggett@gmail.com>
2020-12-16 18:16:02 -0800

    Added unit test projects to the Solution

---
+++ HandlerTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HandlerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
