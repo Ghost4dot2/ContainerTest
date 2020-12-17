024e6f989ed78fc7df72363892dee64559ea682a
Ghost4dot2 <ngroggett@gmail.com>
2020-12-17 09:30:55 -0800

    Initial experiment with RabbitMQ

--- Echo/Program.cs
+++ Echo/Program.cs
@@ -1,5 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
+using System.Text;
 
 
 namespace DBHandler
@@ -7,6 +9,35 @@ namespace DBHandler
     class Program
     {
         static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+
+            }
+        }
+
+        void jsonDataBaseExample()
         {
             Customer customer1 = new Customer();
             customer1.ID = "";
@@ -64,7 +95,6 @@ namespace DBHandler
             jsonDB.debug<Customer>();
             //Customer deserialized = JsonConvert.DeserializeObject<Customer>(output);
             //Console.WriteLine(deserialized.ID);
-
         }
     }
 }
