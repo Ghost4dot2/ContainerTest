261051817d499667f49be706daf6e1084406c530
Ghost4dot2 <ngroggett@gmail.com>
2020-12-16 18:07:46 -0800

    Added a new project to experiment with RabbitMQ before containerizing the projects

---
+++ DBController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
